e4f20a36432e2afd3014a4a16e2f4af5da0b485a
oyoungsun <oyoungsun1030@gmail.com>
2022-04-28 15:35:52 +0900

    solve

--- Maze/src/Main.java
+++ Maze/src/Main.java
@@ -17,104 +17,58 @@ public class Main {
 
     static int[][] dir = { {-1, 0}, {0, -1}, {1, 0},{0, 1}};
     static int[][] maze;
-//    static int[][] maze = {{0,0,0,0,0,0,0},
-//                            {0,1,0,1,1,1,1,0},
-//                            {0,1,0,1,0,1,0,0},
-//                            {0,1,0,1,0,1,1,0},
-//                            {0,1,1,1,0,1,1,0},
-//                            {0,0,0,0,0,0,0,0}};
+
     static int n, m;
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException {
         makeMaze(); //가장자리 0으로 테투리 만들기
-        //System.out.println(Arrays.deepToString(maze));
         System.out.println(findPath(new Node(n,m)));
-
     }
     public static void makeMaze() throws IOException{
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-
-        maze = new int[n+2][m+2];
-        for(int i = 1; i <= n; i++){
+        maze = new int[n][m];
+        for(int i = 0; i < n; i++){
             String input = br.readLine();
-            for(int j = 1; j <= m; j++) {
-                maze[i][j] = input.charAt(j - 1) - '0';
+            for(int j = 0; j < m; j++) {
+                maze[i][j] = input.charAt(j) - '0';
             }
         }
-        for(int i=0; i<=n+1; i++){
-            maze[i][0] = 0;
-            maze[i][m+1] = 0;
-        }
-        for(int j=0; j<=m+1; j++){
-            maze[0][j]=0;
-            maze[n+1][j]=0;
-        }
-    //
-
     }
 
     public static Integer findPath(Node exit){
-        boolean[][] visit;
         Queue<Node> queue = new LinkedList<>();
         int pathcount = 0;
+        boolean[][] visit = new boolean[exit.x][exit.y];
+        visit[0][0] = true;
 
-        visit = new boolean[exit.x+2][];
-        for(int i=0; i<=exit.x+1; i++){
-            visit[i] = new boolean[exit.y+2];
-            visit[i][0] = true;
-            for(int j=1; j<=exit.y; j++)
-                visit[i][j]=false; //visit 초기화
-            visit[i][exit.y+1] = true;
-        }
-        for(int j=0; j<=exit.y+1; j++){
-            visit[0][j]=true;           //visit mapping
-        }
-        for(int j=0; j<=exit.y+1; j++){
-            visit[exit.x+1][j]=true;    //visit mapping
-        }
+        Node now = new Node(0,0);
+        Node next = new Node(0,0);
 
-        Node start = new Node(1,1);
-        int status = 0;
-
-        Node now = new Node(1,1);
-        Node next = new Node(1,1);
-        //now = next = start; 이거 고치기
-        queue.offer(start);
-        while(now.x!=exit.x || now.y!=exit.y){ //x,y둘중에 하나라도 출구 아님
+        queue.add(new Node(0,0));
+        while(!queue.isEmpty()){//now.x!=exit.x || now.y!=exit.y){ //x,y둘중에 하나라도 출구 아님
             now = queue.poll();
-            if(now == null){
-                System.out.println("null");
-                break;
-            }
-            pathcount++;
             visit[now.x][now.y] = true;
-            for(int i=0; i<4; i++){ //여기가 아닌듯
+            for(int i=0; i<4; i++){
                 next.x = now.x + dir[i][0];
                 next.y = now.y + dir[i][1];
-                if(maze[next.x][next.y]==1 && !visit[next.x][next.y]) {//갈 수 있으면
-                   //is next can go?(1) in Maze && not in visit(0)
-                    if(next.x==exit.x&&next.y==exit.y){ //출구와 일치
-                        pathcount++;
-                        System.out.println("exit");
-                        return pathcount;
-                    }
-                    status++; //한가지라도 있을 경우
-                    queue.offer(new Node(next.x,next.y));
+                //출구 거나, 0보다 작아지는 경우
+                if(next.x<0||next.y<0||next.x>=exit.x||next.y>= exit.y){
+                    continue;
                 }
-                visit[next.x][next.y] = true;
+                if(visit[next.x][next.y]||maze[next.x][next.y]==0){
+                    continue;
+                }
+                    queue.add(new Node(next.x,next.y));
+                    maze[next.x][next.y] = maze[now.x][now.y] + 1;
+                    visit[next.x][next.y] = true; //두줄이 이해가 안됨
+                //}
             } //dir모두 방문함
-            if(status == 0){
-                pathcount--; //방문할 가지가 없는 경우 방문 안한 것으로 침
-            }
-
         }
-        System.out.println("can't find exit");
-        return 0;
+        return maze[exit.x-1][exit.y-1];
     }
 }
 
